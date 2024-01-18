22aa926a09659c2176b8a4256c8ed05a535b8e1c
ascarbek <ascarbek@gmail.com>
2024-01-18 11:13:54 +0600

    more config, include fa

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({
+    postcss: true,
+  }),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    alias: {
+      '$srv/*': 'src/lib/server/*',
+      '$components/*': 'src/lib/components/*',
+      '$data/*': 'src/lib/data/*',
+      '$api/*': 'src/lib/components/_api/*',
+    },
+  },
 };
 
 export default config;
